e52fce5b04b34518687767be76772c65c2031f01
allyson_s <allysonsmithstudio@gmail.com>
2022-06-11 20:10:32 -0400

    Updated data format

--- js/main.js
+++ js/main.js
@@ -1,10 +1,43 @@
+const chartData = [
+  {
+    day: "mon",
+    amount: 17.45,
+  },
+  {
+    day: "tue",
+    amount: 34.91,
+  },
+  {
+    day: "wed",
+    amount: 52.36,
+  },
+  {
+    day: "thu",
+    amount: 31.07,
+  },
+  {
+    day: "fri",
+    amount: 23.39,
+  },
+  {
+    day: "sat",
+    amount: 43.28,
+  },
+  {
+    day: "sun",
+    amount: 25.48,
+  },
+];
+
+const labels = chartData.map((data) => data.day);
+const amount = chartData.map((data) => data.amount);
 const canvasElement = document.getElementById("myChart");
 
 const data = {
-  labels: ["mon", "tues", "wed", "thu", "fri", "sat", "sun"],
+  labels: labels,
   datasets: [
     {
-      data: [17.45, 34.91, 52.36, 31.07, 23.39, 43.28, 25.48],
+      data: amount,
       backgroundColor: [
         "hsl(10, 79%, 65%)",
         "hsl(10, 79%, 65%)",
